425667b47bdffc1d7c0e8b9968098b74e2efed89
zwy426 <wenyu.zwy@gmail.com>
2015-06-11 01:44:48 +0800

    dtw w clustering

--- DTWsubseqmatch.py
+++ DTWsubseqmatch.py
@@ -8,14 +8,14 @@ import Plot
 
 A variation of subsequence DTW on time-series s,t (misaligned, periodic).
 s query, t reference
-For trunc (~ period/2), truncate s at the start and t at the end by {1,...trunc} units,
+For trunc (~ period), truncate s at the start and t at the end by {1,...trunc} units,
 and returns the best distance and the corresponding truncation.
 
 ref: http://www.jstatsoft.org/v31/i07/paper
 
 '''
 
-def DTWsubseq(s, t, trunc = 10, bandwidth = 10, pnorm = 1):
+def DTWsubseq(s, t, trunc = 25, bandwidth = 5, pnorm = 1):
     best_trunc = 0
     best_dist = np.inf
     for i in range(trunc):
@@ -28,50 +28,62 @@ def DTWsubseq(s, t, trunc = 10, bandwidth = 10, pnorm = 1):
 
 if __name__ == "__main__":
 
-    #Xtrain = np.load('../6780Project/Xtrain.npy')
-    #Ytrain = np.load('../6780Project/Ytrain.npy')
+    Xtrain = np.load('xTrainC.npy')
+    Ytrain = np.load('yTrainC.npy')
 
-    Xtrain = np.load('../Data/Xtrain.npy')
-    Ytrain = np.load('../Data/Ytrain.npy')
+    dist_mats = []
+    trunc_mats = []
 
-    # activity 0
-    Xtrain0 = Xtrain[Ytrain==0][:100]
-    Ytrain0 = Ytrain[Ytrain==0][:100]
-    n = Xtrain0.shape[0]
+    for act in range(6):
+        Xtrain_act = Xtrain[Ytrain==act]
+        Ytrain_act = Ytrain[Ytrain==act]
+        n = Xtrain_act.shape[0]
 
-    dist_mat = np.zeros((n,n))
-    trunc_mat = np.zeros((n,n))
+        dist_mat = np.zeros((n,n))
+        trunc_mat = np.zeros((n,n))
 
-    for i in range(n):
-        for j in range(n):
-            if j>i:
-                print 'row'+str(i)+' column'+str(j)
-                best_trunc, best_dist = DTWsubseq(Xtrain0[j],Xtrain0[i])
-                trunc_mat[i,j] = best_trunc
-                dist_mat[i,j] = best_dist
-                print best_dist
+        for i in range(n):
+            for j in range(n):
+                if j>i:
+                    print 'act'+str(act)+'row'+str(i)+' column'+str(j)
+                    best_trunc, best_dist = DTWsubseq(Xtrain_act[j],Xtrain_act[i])
+                    trunc_mat[i,j] = best_trunc
+                    dist_mat[i,j] = best_dist
 
-    #np.save('dist_mat.npy', dist_mat)
-    #np.save('trunc_mat.npy', trunc_mat)
+        for i in range(n):
+            for j in range(n):
+                if j<i:
+                    dist_mat[i,j] = dist_mat[j,i]
 
-    for i in range(n):
-        for j in range(n):
-            if j<i:
-                dist_mat[i,j] = dist_mat[j,i]
+    dist_mats.append(dist_mat)
+    trunc_mats.append(trunc_mat)
 
-    np.save('dist_mat.npy', dist_mat)
-    np.save('trunc_mat.npy', trunc_mat)
+    np.save('dist_mats.npy', dist_mats)
+    np.save('trunc_mats.npy', trunc_mats)
+
+'''
+    # Hierarchical clustering
+
+    Xtrain = np.load('xTrainC.npy')
+    Ytrain = np.load('yTrainC.npy')
 
     dist_mat = np.load('dist_mat.npy')
     trunc_mat = np.load('trunc_mat.npy')
 
-    # hierachical clustering
-    data_link = linkage(dist_mat, method='complete')
-    ind = fcluster(data_link, 0.7*data_link.max(),criterion='distance')
-    num_cluster = max(ind) # number of clusters for activity
-    cluster = [] # clusters
-    for c in range(1,num_cluster+1):
-        cluster.append(Xtrain0[ind==c])
+    cluster = [] # cluster for all activities
+    ycluster = [] # cluster label
+    for act in range(6):
+        Xtrain_act = Xtrain[Ytrain==act]
+        data_link = linkage(dist_mat[act], method='complete')
+        ind = fcluster(data_link, 0.7*data_link.max(), criterion='distance')
+        num_cluster = max(ind) # number of clusters for activity
+        for c in range(1,num_cluster+1):
+            cluster.append(Xtrain_act[ind==c])
+            ycluster.append(act)
 
-    for c in range(1,num_cluster+1):
-        Plot.plot_raw(cluster[c-1], Ytrain0[ind==c], 'plot/hcluster'+str(c), 5, act=1, file=False)
+    num_c = ycluster.__len__()
+    for c in range(num_c):
+        Plot.plot_raw(cluster[c], np.array(ycluster[c]*cluster[c].shape[0]),
+                      'plot/act'+str(ycluster[c])+'_hcluster'+str(c), 5, act=1, file=False)
+
+'''
