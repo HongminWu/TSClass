7b96e17ab0caf0bc8319a11ee77f83e02dadc692
ss3349 <ss3349@dss1.cac.cornell.edu>
2015-06-23 11:06:24 -0400

    adding svm classification

--- Classify.py
+++ Classify.py
@@ -3,7 +3,7 @@ from numpy import linalg as la
 import TimeSeriesMethods as ts
 import random as r
 clusters = np.load("cluster.npy")
-cluster_labels = np.load("ycluster10.npy")
+cluster_labels = np.load("ycluster.npy")
 
 
 def buildTemplates():
@@ -18,16 +18,44 @@ def buildTemplates():
 		cluster_templates_aligned.append(ts.average(l))
 	return cluster_templates_aligned
 
-
+def classifySVM(file1,file2,num):
+	dist_features_test = np.load("distances_hclust_average_new_dist.npy")
+	xTrain = np.load(file1)
+	yTrain=np.load(file2)
+	yTest = np.load("yTestC.npy")
+	cluster_temps = np.load("cluster_templates_hclust_average_new_dist.npy")
+	dist_features_train = []
+	for k in xTrain:
+		dist = []
+		for temp in cluster_temps:
+			dist.append(ts.DTWsubseq(k,temp)[1])
+		dist_features_train.append(dist)
+	np.save("distances_hclust_average_new_dist.npy")
+        #Xtest = pca.transform(feature_test)
+	y_pred =  svm.LinearSVC().fit(dist_features_train,label_train).predict(distance_features_test)
+	cm = confusion_matrix(label_test,y_pred)
+	s=svm.LinearSVC().fit(dist_features_train, yTrain).score(dist_features_test, yTest)
+        return cm, s
+	
 #file 1 is time series, file 2 is labels
 #saves confusion matrix
 #outputs confusion amtrix, accuracy
-def classify(file1,file2,num):
+def getDistances(file1,file2,num):
 	testTS = np.load(file1)
 	test_labels = np.load(file2)
-	#cluster_templates = buildTemplates()
-	cluster_templates = np.load("dba_templatees.npy")
+	cluster_templates = buildTemplates()
+	np.save("cluster_templates_hclust_average_new_dist.npy",cluster_templates)
+	#cluster_templates = np.load("dba_templates.npy")
 	#build predited labels
+	'''
+	for k in range(len(test_labels)):
+	    if test_labels[k]>2:
+		test_labels[k]=3
+	for w in range(len(cluster_labels)):
+	    if cluster_labels[w]>2:
+		cluster_labels[w]=3
+	'''
+        distancefeatures = []
 	predicted_labels = []
 	i=0
 	for sample in testTS:
@@ -35,11 +63,13 @@ def classify(file1,file2,num):
 		print "sample", i
 		for template in cluster_templates:
 			dist.append(ts.DTWsubseq(sample,template)[1])
-		predicted_labels.append(cluster_labels[np.argmin(dist)])
+		distancefeatures.append(dist)
+		#predicted_labels.append(cluster_labels[np.argmin(dist)])
 		i+=1
-	np.save("predicted_labels_hclust_average_new_dist.npy", predicted_labels)
+	np.save("distances_hclust_average_new_dist.npy",distancefeatures)
+	np.save("predicted_labels_hclust_average_new_dist_less_labels.npy", predicted_labels)
 	#predicted_labels = np.load("predicted_labels_hclust_average_lessweird.npy")
-
+	'''
 	#accuracy and confusion matrix
 	accuracy = 0
 	confusion_matrix = np.zeros(shape=(6,6))	
@@ -49,21 +79,13 @@ def classify(file1,file2,num):
 		if predicted_labels[p] == (test_labels[p]):
 			accuracy+=1
 
-	np.save("confusion_matirx_hclust_new_dist"+str(num)+".npy",confusion_matrix)
+	np.save("confusion_matirx_hclust_new_dist_less_labels"+str(num)+".npy",confusion_matrix)
 	return confusion_matrix, float(accuracy)/len(test_labels)
-
-
-print classify("xTestC_1.npy", "yTestC_1.npy",1)
-# np.save("yTestC_1.npy",np.load("yTestC.npy")[0:int(len(np.load("yTestC.npy"))/5)])
-# np.save("yTestC_2.npy",np.load("yTestC.npy")[int(len(np.load("yTestC.npy"))/5):int(len(np.load("yTestC.npy"))*2/5)])
-# np.save("yTestC_3.npy",np.load("yTestC.npy")[int(len(np.load("yTestC.npy"))*2/5):int(len(np.load("yTestC.npy"))*3/5)])
-# np.save("yTestC_4.npy",np.load("yTestC.npy")[int(len(np.load("yTestC.npy"))*3/5):int(len(np.load("yTestC.npy"))*4/5)])
-# np.save("yTestC_5.npy",np.load("yTestC.npy")[int(len(np.load("yTestC.npy"))*4/5):int(len(np.load("yTestC.npy")))])
-
-
-
-
-
-
-
-	
+	'''
+print getDistances("xTestC.npy","yTestC.npy",0)
+#print classify("PUCK_xTestC_5.npy", "PUCK_yTestC_5.npy",5)
+#np.save("PUCK_xTestC_1.npy",np.load("PUCK_xTestC.npy")[0:int(len(np.load("PUCK_xTestC.npy"))/5)])
+#np.save("PUCK_xTestC_2.npy",np.load("PUCK_xTestC.npy")[int(len(np.load("PUCK_xTestC.npy"))/5):int(len(np.load("PUCK_xTestC.npy"))*2/5)])
+#np.save("PUCK_xTestC_3.npy",np.load("PUCK_xTestC.npy")[int(len(np.load("PUCK_xTestC.npy"))*2/5):int(len(np.load("PUCK_xTestC.npy"))*3/5)])
+#np.save("PUCK_xTestC_4.npy",np.load("PUCK_xTestC.npy")[int(len(np.load("PUCK_xTestC.npy"))*3/5):int(len(np.load("PUCK_xTestC.npy"))*4/5)])
+#np.save("PUCK_xTestC_5.npy",np.load("PUCK_xTestC.npy")[int(len(np.load("PUCK_xTestC.npy"))*4/5):int(len(np.load("PUCK_xTestC.npy")))])	
--- FeatureClassification.py
+++ FeatureClassification.py
@@ -5,32 +5,36 @@ from sklearn.decomposition import PCA
 import sklearn.tree as tree
 import FeatureGeneration as fg
 import Reader as r
+from sklearn.metrics import confusion_matrix
 
-#feature_train = fg.featureExtraction("UCI_HAR_all_multivariateTS.npy", "X_train_UCI_features.npy")
-#feature_test = fg.featureExtraction("UCI_HAR_all_test_multTS.npy", "X_test_UCI_features.npy")
-#np.save("Xtrain_features.npy", feature_train)
-#np.save("Xtest_features.npy", feature_test)
-feature_train = np.load("Xtrain_features.npy")
-feature_test = np.load("Xtest_features.npy")
+feature_train = fg.featureExtraction("PUCK_xTrainC.npy", "X_train_UCI_features.npy")
+feature_test = fg.featureExtraction("PUCK_xTestC.npy", "X_test_UCI_features.npy")
+np.save("Xtrain_features_puck.npy", feature_train)
+np.save("Xtest_features_puck.npy", feature_test)
+feature_train = np.load("Xtrain_features_puck.npy")
+feature_test = np.load("Xtest_features_puck.npy")
 
-label_test = np.load("UCI_HAR_test_labels.npy")
-label_train = np.load("UCI_HAR_train_labels.npy")
+label_test = np.load("PUCK_yTestC.npy")
+label_train = np.load("PUCK_yTrainC.npy")
+'''
+for k in range(len(label_train)):
+	if label_train[k]>2:
+		label_train[k]=3
+for l in range(len(label_test)):
+	if label_test[l]>2:
+		label_test[l]=3
+'''
+
 j = 350
 k = 0
-s = []
-while j<400:
-	w = []
-	k=0
-	while k<10:
-		pca = PCA(n_components=j)    
-		Xtrain = pca.fit(feature_train).transform(feature_train)
+pca = PCA(n_components=j)    
+Xtrain = pca.fit(feature_train).transform(feature_train)
 
-		Xtest = pca.transform(feature_test)
-		w.append(svm.LinearSVC().fit(Xtrain, label_train).score(Xtest, label_test))
-		k+=1
-	s.append((max(w),j))
-	j+=2
-print s
+Xtest = pca.transform(feature_test)
+y_pred =  svm.LinearSVC().fit(Xtrain,label_train).predict(Xtest)
+cm = confusion_matrix(label_test,y_pred)
+print cm
+print svm.LinearSVC().fit(Xtrain, label_train).score(Xtest, label_test)
 
 
 
