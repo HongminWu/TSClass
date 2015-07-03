d22ecbba41db5e3e1de7b4ccde2ae202dfeade1e
Zeratulaz <zycsiriuz@Gmail.com>
2015-07-03 09:15:04 +0800

    ARCoef refined

--- ARCoef.py
+++ ARCoef.py
@@ -1,15 +1,18 @@
-from sklearn import linear_model as lm
+from sklearn import linear_model
 import numpy as np
 
 def ARCoef(x, bandwidth = 8):
-	autoReg = lm.LinearRegression()
+	autoReg = linear_model.LinearRegression()
 	X = []
 	Y = []
 	for i in range(bandwidth, len(x)):
 		Y.append(x[i])
-		X.append([x[j] for j in range(i-bandwidth, i)])
+		temp = list([1])
+		for j in range(i-bandwidth, i):
+			temp.append(x[j])
+		X.append(temp)
 	autoReg.fit(X, Y)
 	return list(autoReg.coef_)
 
-
-
+# X = [1,1,2,3,5,8,13,21]
+# print ARCoef(X, 2)
